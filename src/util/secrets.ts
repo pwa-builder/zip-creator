54157c0b26f159066f5d242297d9cb4441f1bc24
lee-leonardo <lee.leonardo.king@gmail.com>
2020-04-21 14:36:34 -0700

    rm mongo ref

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -10,12 +10,8 @@ if (fs.existsSync(".env")) {
   dotenv.config({ path: ".env.example" }); // you can delete this after you create your own .env file!
 }
 export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const MONGODB_URI = prod
-  ? process.env["MONGODB_URI"]
-  : process.env["MONGODB_URI_LOCAL"];
 
 if (!SESSION_SECRET) {
   logger.error("No client secret. Set SESSION_SECRET environment variable.");
