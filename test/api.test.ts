a1b751edf1652a49737650d185458feea616596f
lee-leonardo <lee.leonardo.king@gmail.com>
2020-04-21 13:52:16 -0700

    whitespace format, rm get for now

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,9 +1,14 @@
 import request from "supertest";
 import app from "../src/app";
 
-describe("GET /api", () => {
-    it("should return 200 OK", () => {
-        return request(app).get("/api")
-            .expect(200);
-    });
+// describe("GET /api", () => {
+//   it("should return 200 OK", () => {
+//     return request(app).get("/api").expect(200);
+//   });
+// });
+
+describe("POST /api", () => {
+  it("should return 200 OK", () => {
+    return request(app).get("/api").expect(200);
+  });
 });
--- test/app.test.ts
+++ test/app.test.ts
@@ -2,8 +2,7 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /random-url", () => {
-    it("should return 404", (done) => {
-        request(app).get("/reset")
-            .expect(404, done);
-    });
+  it("should return 404", (done) => {
+    request(app).get("/reset").expect(404, done);
+  });
 });
